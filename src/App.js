8269b991a68449cadac35ed5193c5c2cdfb02cde
omar <omar.salah@gmail.com>
2022-07-10 18:40:59 +0200

    added my first project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("")
+  
+  const ops = ["*", "/", "+", "-", "."]
+  
+  const update = value => {
+    if (
+      (ops.includes(value) && calc === "") ||
+      (ops.includes(value) && ops.includes(calc.slice(-1)))
+    ) {
+      return;
+    }
+
+    setCalc(calc + value)
+
+    if (!ops.includes(value)) {
+      setResult(eval(calc + value).toString())
+    }
+  }
+
+  const equalEffect = () => {
+    setCalc(result.toString())
+    setResult("")
+  }
+
+  const delEffect = () => {
+    setCalc(eval(calc.slice(0, -1)))
+    setResult(calc.slice(0, -1))
+  }
+
+  const clearEffect = () => {
+    setCalc("")
+    setResult("")
+  }
+
+  const nine = () => {
+    const digits = []
+    for (let i = 1;i<10;i++) {
+      digits.push(
+      <button 
+        onClick={() => update(i.toString())} 
+        key={i}>{i}
+      </button>)
+    }
+
+    return digits
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+        <div className="display">
+          { result ? <span>({result})</span>: "" }{ calc || "0" }
+        </div>
+
+        <div className="operators">
+          <button onClick={() => update("+")}>+</button>
+          <button onClick={() => update("-")}>-</button>
+          <button onClick={() => update("*")}>*</button>
+          <button onClick={() => update("/")}>/</button>
+          <button onClick={delEffect}>Del</button>
+        </div>
+
+        <div className="digits">
+          {nine()}
+          <button onClick={() => update("0")}>0</button>
+          <button onClick={() => update(".")}>.</button>
+          <button onClick={() => equalEffect()}>=</button>
+        </div>
+        <button onClick={clearEffect} className="clear">Clear All</button>
+      </div>
     </div>
   );
 }
